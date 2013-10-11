71896285a7573f97971758119a7c23c32f159007
dobachi <devnull@localhost>
2013-10-11 10:06:56 +0900

    storm検証環境を一度に立ち上げ、止めるためのスクリプト

---
+++ start_storm_env.sh
@@ -0,0 +1,3 @@
+(cd storm_cluster; vagrant up)
+(cd kafka_cluster; vagrant up)
+(cd hbase_cluster; vagrant up hbase-pseudo)
---
+++ stop_storm_env.sh
@@ -0,0 +1,3 @@
+(cd hbase_cluster; vagrant halt hbase-pseudo)
+(cd kafka_cluster; vagrant halt)
+(cd storm_cluster; vagrant halt)
