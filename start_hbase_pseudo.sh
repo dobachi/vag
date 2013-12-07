52e2223a509ba561775343bfe02d68d3d367a004
dobachi1983@gmail.com <dobachi1983@gmail.com>
2013-12-08 00:42:14 +0900

    HBaseの疑似分散環境を起動／停止するためのスクリプトを追加

---
+++ start_hbase_pseudo.sh
@@ -0,0 +1,3 @@
+(cd management; vagrant up)
+(cd storm_cluster; vagrant up)
+(cd hbase_cluster; vagrant up hbase-pseudo-cdh3)
---
+++ stop_hbase_pseudo.sh
@@ -0,0 +1,3 @@
+(cd storm_cluster; vagrant halt)
+(cd hbase_cluster; vagrant halt hbase-pseudo-cdh3)
+(cd management; vagrant halt)
